16443bb44423b4f16dfc0a176093018f5bad2128
Roland Levy <rolandjlevy@gmail.com>
2020-07-15 09:05:17 +0000

    Added button

---
+++ notes.md
@@ -0,0 +1,4 @@
+// create img element
+const imgWrapper = document.querySelector('.wrapper');
+const img = document.createElement('img');
+imgWrapper.appendChild(img);
--- script.js
+++ script.js
@@ -2,16 +2,22 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
   const url = `https://api.unsplash.com/photos/random?client_id=${key}&w=300`;
 
-  getImage(url).then(data => {
-    const { width, height, description, urls, links } = data;
-    const imgWrapper = document.querySelector('.img-wrapper');
-    const img = document.createElement('img');
-    imgWrapper.appendChild(img);
-    imgWrapper.innerHTML = `<img src=${urls.regular} width="300">`;
-  }).catch(error => {
-    console.log({error});
+  const img = document.querySelector('.random-img'); 
+  img.width = '300';
+
+  const btn = document.querySelector('.btn'); 
+
+  btn.addEventListener('click', (e) => {
+    getImage(url).then(data => {
+      const { width, height, description, urls, links } = data;
+      img.src = urls.regular;
+    }).catch(error => {
+      console.log({error});
+    });
   });
 
+  btn.click();
+  
   function getImage(url) {
     return new Promise((resolve, reject) => {
       fetch(url)
